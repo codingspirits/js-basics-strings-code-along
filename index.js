5e742b193f77afab47163a71a33d1b201e4b0ba3
codingspirits <vincci.cheng.9@gmail.com>
2021-07-13 15:19:40 +1000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+let currentUser = "Vincci"
+let welcomeMessage = `Welcome to Flatbook, ${currentUser} !`
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser} !`
+let shortGreeting = `Welcome, ${currentUser[0]}!`
+console.log(shortGreeting)
